b1922d368f8c0a7b61b01b9afb20d36b75a4fa43
joelmoxon <moxonjoeljames@gmail.com>
2026-01-09 14:11:40 +0000

    Add job card

--- my_menu_app/lib/job_list_screen.dart
+++ my_menu_app/lib/job_list_screen.dart
@@ -7,16 +7,50 @@ class JobListScreen extends StatefulWidget {
 }
 
 class _JobListScreenState extends State<JobListScreen> {
+  List<Map<String, dynamic>> jobs = [
+    {
+      'id':1,
+      'title': 'Replace hydraulic fluid',
+      'priority': 'high',
+      'status': 'open',
+      'created_at': '09-01-2026',
+    },
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('RampCheck')),
-      body: const Center(
-        child: Text(
-          'No jobs found.\nTap + to create a new job',
-          textAlign: TextAlign.center,
-          style: TextStyle(fontSize: 16, color: Colors.grey),
-        ),
+      body: jobs.isEmpty
+        ? const Center(
+          child: Text(
+            'No jobs found.\nTap + to create a new job',
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 16, color: Colors.grey),
+          ),
+        )
+      : ListView.builder(
+          padding: const EdgeInsets.all(8),
+          itemCount: jobs.length,
+          itemBuilder: (context, index) {
+            final job = jobs[index];
+            return Card(
+              margin:const EdgeInsets.symmetric(vertical: 6, horizontal: 4),
+              elevation: 2,
+              child: ListTile(
+                title: Text(
+                  job['title'],
+                  style: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16,
+                  ),
+                ),
+                trailing:const Icon(Icons.chevron_right),
+                onTap: () {
+                  print("Selected job:${job['title']}");
+                },
+              )
+            );
+          }
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
